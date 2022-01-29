e06c9b214d45cb0ce6c938a88c89b6b619be482b
Karol Kolanski <gralu86@gmail.com>
2022-01-29 14:56:06 +0100

    First script

--- test_selenium.py
+++ test_selenium.py
@@ -1,8 +1,20 @@
 # Import biblioteki
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Otwarcie przeglądarki
 driver = webdriver.Firefox()
 # driver = webdriver.Chrome()
-#Otwarcie strony wp.pl
-driver.get("http://www.wp.pl")
+
+#Otwarcie strony duckduckgo.com
+driver.get("http://duckduckgo.com")
+
+# Maksymalizacja okna
+driver.maximize_window()
+
+# Odszukanie elementu - pole wyszukiwania
+element = driver.find_element_by_id('search_form_input_homepage')
+print(type(element))
+print(element.tag_name)
+
+driver.quit()
