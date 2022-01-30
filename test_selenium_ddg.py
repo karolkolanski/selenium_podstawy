fa59af22f4600e3139353a06bf4cff91104faa5e
Karol Kolanski <gralu86@gmail.com>
2022-01-30 10:42:05 +0100

    implicit wait

--- test_selenium_ddg.py
+++ test_selenium_ddg.py
@@ -13,13 +13,19 @@ driver.maximize_window()
 
 # Odszukanie elementu
 element = driver.find_element(By.ID, 'search_form_input_homepage')
-element.send_keys("tester ALK")
+element.send_keys("tester kozminski")
 element.submit()
 
 # "lamerskie" rozwiązanie
-sleep(3)
+sleep(4)
+
+# Bardziej profesjonalne rozwiązanie
+# Maksymalny czas oczekiwania na element
+# (jeśli znajdzie szybcoiej, to poleci dalej)
+driver.implicitly_wait(40)
 # find_elements zwraca listę elementów
 elements = driver.find_elements(By.XPATH, '//h2[@class="result__title js-result-title"]')
+driver.implicitly_wait(0)
 
 for el in elements:
     print(el.text)
