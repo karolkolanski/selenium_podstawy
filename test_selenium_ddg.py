0fc76aa1e74cb76076b8d7f6fd0d0b941592cbe2
Karol Kolanski <gralu86@gmail.com>
2022-01-30 10:30:26 +0100

    Seatching for tester alk

--- test_selenium_ddg.py
+++ test_selenium_ddg.py
@@ -16,5 +16,13 @@ element = driver.find_element(By.ID, 'search_form_input_homepage')
 element.send_keys("tester ALK")
 element.submit()
 
+# "lamerskie" rozwiązanie
+sleep(3)
+# find_elements zwraca listę elementów
+elements = driver.find_elements(By.XPATH, '//h2[@class="result__title js-result-title"]')
+
+for el in elements:
+    print(el.text)
 # Śpię 3 sekundy, aby zobaczyć co się stało
 sleep(3)
+
